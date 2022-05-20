640a7bbb2dcb443feb20c892a0dafa0b7476de60
nishiurahiroki <nishiurahiroki@gmail.com>
2022-05-20 13:41:10 +0000

    Create _app and _document

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+function MyApp({Component, pageProps}) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+      </Head>
+      <Component {...pageProps}/>
+    </>
+  )
+}
+
+export default MyApp
