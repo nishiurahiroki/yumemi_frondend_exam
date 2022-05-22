e68c5899c61f06d92f311efc87b6fd59dff49ae2
nishiurahiroki <nishiurahiroki@gmail.com>
2022-05-22 06:56:05 +0000

    リファクタリング usePrefections.ts内の重複していた記述を改善

--- src/hooks/usePrefections.ts
+++ src/hooks/usePrefections.ts
@@ -1,18 +1,11 @@
 import useSWR from "swr"
 
-import { RESAS_API_HOST, RESAS_API_GET_PREFECTURES } from '../const'
+import { getPrefections } from "../repositories/prefectures"
 
-const fetcher = (url : string) => fetch(url, {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY
-  }
-})
-.then(res => res.json())
+const fetcher = () => getPrefections()
 
 export default function usePrefections(fallbackData) {
-  const { data, error, mutate } = useSWR(RESAS_API_HOST + '/' + RESAS_API_GET_PREFECTURES, fetcher, { suspense: true, fallbackData })
+  const { data, error, mutate } = useSWR('getPrefections', fetcher, { suspense: true, fallbackData })
 
   return {
     prefs : data?.result,
